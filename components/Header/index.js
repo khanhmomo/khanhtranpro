d11a8152a23440aa388b21b623874950c6d935fc
Khanh Tran <ankhanhct@gmail.com>
2022-09-20 03:06:29 -0500

    F

--- components/Header/index.js
+++ components/Header/index.js
@@ -64,7 +64,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
             </div>
             <Popover.Panel
               className={`absolute right-0 z-10 w-11/12 p-4 ${
-                theme === "dark" ? "bg-slate-800" : "bg-white"
+                theme === "dark" ? "bg-slate-800" : "bg-slate-800"
               } shadow-md rounded-md`}
             >
               {!isBlog ? (
